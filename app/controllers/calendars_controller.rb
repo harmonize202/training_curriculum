9fe15f8b202c834f7981f54c9f53124e715e7647
harmonize202 <78188377+harmonize202@users.noreply.github.com>
2021-02-09 19:31:13 +0900

    Issue6

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -34,7 +34,15 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
+
+      # 曜日に対応する数字が入る(日曜日：０、、、土曜日：６)
+      wday_num = Date.today.wday
+      # ７以上になると０に戻る
+      if wday_num + x >= 7
+        wday_num = wday_num - 7
+      end
+
+      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans, wdays: (wdays[wday_num + x])}
       @week_days.push(days)
     end
 
